9c85a9a936ede5ba3f45ba3023d2322e6545260c
ehoferreira <ferreiraohara@gmail.com>
2024-11-16 20:15:32 -0300

    Atualiza pagina index

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1> {"O universo sem você é o raio cósmico que ilumina meu pequeno planeta <3"} </h1>;
+    return <h1> {"O universo sem você é um abismo incomprensível, você é raio cósmico que ilumina meu pequeno planeta <3"} </h1>;
 }   
 export  default Home;
